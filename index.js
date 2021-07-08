d3dc203933bedab010739b4d8a605015f64250b8
carmenfu006 <guess_006@hotmail.com>
2021-07-08 13:03:22 +0800

    Change arrow function to function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // window.addEventListener('load', (event) => {
 //   // insert code here
+     // console.log(this)
 // });
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', function() {
   let button = document.getElementById('button');
   let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let passcode = document.querySelector('.passcode');
